7c22340a8460bcabdac0d1a0ba50224785a387d7
DanielAlten129 <daniel.colinas@alten.es>
2025-06-06 13:25:03 +0200

    Update MyClass.java

--- src/main/java/nl/tudelft/mini/project/MyClass.java
+++ src/main/java/nl/tudelft/mini/project/MyClass.java
@@ -1,5 +1,8 @@
 package nl.tudelft.mini.project;
 
-public class MyClass {
-
+public class MainApp {
+    public static void main(String[] args) {
+        System.out.println("Aplicación arrancada correctamente");
+        // Aquí puedes llamar a métodos de otras clases si quieres
+    }
 }
